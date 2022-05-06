3c12cb4b6af20ab76762a55fb2cc775f1e2bbb00
EmerickD <74702547+Xeki-D@users.noreply.github.com>
2022-05-06 16:11:25 +0200

    Add files via upload

--- View/MainWindow.xaml.cs
+++ View/MainWindow.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using WpfApp_NEKOINU.ViewModel;
+using MySql.Data.MySqlClient;
 
 namespace WpfApp_NEKOINU.View
 {
@@ -21,7 +21,9 @@ namespace WpfApp_NEKOINU.View
     /// </summary>
     public partial class MainWindow : Window
     {
-        ClientViewModel cvm = new ClientViewModel();
+        string connectionString = "Server=localhost;Database=nekoinu_bdd;Uid=root;Pwd=";
+        bool connecte = false;
+        MySqlConnection connection;
         public MainWindow()
         {
             InitializeComponent();
@@ -29,10 +31,40 @@ namespace WpfApp_NEKOINU.View
 
         private void SignInButton_Click(object sender, RoutedEventArgs e)
         {
-            var login = txb_login.Text;
-            var mdp = txb_mdp.Text;
+            connection = new MySqlConnection(connectionString);
+            try
+            {
 
-            cvm.getLeClient(login, mdp);
+                if (connection.State == System.Data.ConnectionState.Closed)
+                {
+                    connection.Open();
+                    connecte = true;
+                    string query = "Select Count(1) FROM client WHERE ADR_CLIENT=@adr AND MDP_CLIENT=@mdp";
+                    MySqlCommand cmd = new MySqlCommand(query, connection);
+                    MySqlParameter adr = new MySqlParameter("@adr", txb_login.Text);
+                    MySqlParameter mdp = new MySqlParameter("@mdp", txb_mdp.Password);
+                    int count = Convert.ToInt32(cmd.ExecuteScalar());
+                    if(count == 1)
+                    {
+
+                       
+                        SelectionWindows select = new SelectionWindows();
+                        select.Show();
+                        this.Close();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Identifiant incorrect");
+                    }
+
+
+                }
+            }
+            catch (Exception co)
+            {
+                MessageBox.Show(co.ToString());
+            }
+
         }
 
         private void txb_login_TextChanged(object sender, TextChangedEventArgs e)
@@ -44,5 +76,7 @@ namespace WpfApp_NEKOINU.View
         {
 
         }
+
+       
     }
 }
--- View/SecondWindow.xaml.cs
+++ View/SecondWindow.xaml.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,8 +9,11 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 using WpfApp_NEKOINU.ViewModel;
+using MySql.Data.MySqlClient;
+
 
 namespace WpfApp_NEKOINU.View
 {
@@ -20,7 +22,10 @@ namespace WpfApp_NEKOINU.View
     /// </summary>
     public partial class SecondWindow : Window
     {
-        AddProduit addP = new AddProduit();
+        string connectionString = "Server=localhost;Database=nekoinu_bdd;Uid=root;Pwd=";
+        bool connecte = false;
+        MySqlConnection connection;
+
         public SecondWindow()
         {
             InitializeComponent();
@@ -28,14 +33,71 @@ namespace WpfApp_NEKOINU.View
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            var nom = txb_nameP.Text;
+            /*var nom = txb_nameP.Text;
             var prix = Double.Parse(txb_prixP.Text);
             var stock = Int32.Parse(txb_stockP.Text);
             var desc = txb_descP.Text;
             var img = txb_imgP.Text;
             var animal = Int32.Parse(txb_animalP.Text);
 
-            addP.getLeProduit(nom, prix, stock, desc, img, animal);
+            addP.getLeProduit(nom, prix, stock, desc, img, animal);*/
+
+            connection = new MySqlConnection(connectionString);
+
+            try
+            {
+                if (connection.State == System.Data.ConnectionState.Closed)
+                {
+                    connection.Open();
+                    connecte = true;
+
+                    
+                    try
+                    {
+                        string query = "INSERT INTO produit (NOM_PRODUIT,PRIX_PRODUIT,STOCK_PRODUIT,DESC_PRODUIT,IMG_PRODUIT,ANIMAL_PRODUIT) VALUES(@nom, @prix, @stock, @desc, @img, @animal)";
+                        MySqlCommand cmd = new MySqlCommand(query, connection);
+                        MySqlParameter nomParam = new MySqlParameter("@nom", MySqlDbType.Text, 100);
+                        MySqlParameter prixParam = new MySqlParameter("@prix", MySqlDbType.Double, 100);
+                        MySqlParameter stockParam = new MySqlParameter("@stock", MySqlDbType.Int32, 100);
+                        MySqlParameter descParam = new MySqlParameter("@desc", MySqlDbType.Text, 100);
+                        MySqlParameter imgParam = new MySqlParameter("@img", MySqlDbType.Text, 100);
+                        MySqlParameter animalParam = new MySqlParameter("@animal", MySqlDbType.Int32, 1);
+
+                        nomParam.Value = txb_nameP.Text;
+                        prixParam.Value = txb_prixP.Text;
+                        stockParam.Value = txb_stockP.Text;
+                        descParam.Value = txb_descP.Text;
+                        imgParam.Value = txb_imgP.Text;
+                        animalParam.Value = txb_animalP.Text;
+
+
+                        cmd.Parameters.Add(nomParam);
+                        cmd.Parameters.Add(prixParam);
+                        cmd.Parameters.Add(stockParam);
+                        cmd.Parameters.Add(descParam);
+                        cmd.Parameters.Add(imgParam);
+                        cmd.Parameters.Add(animalParam);
+
+                        // Call Prepare after setting the Commandtext and Parameters.
+                        cmd.Prepare();
+                        cmd.ExecuteNonQuery();
+
+                        
+                    }
+                    catch (MySqlException co)
+                    {
+                        Console.WriteLine("Error Generated. Details: " + co.ToString());
+                    }
+                }
+                connection.Close();
+                MessageBox.Show("Insertion produit validé");
+                this.Close();
+            }
+            catch (MySqlException co)
+            {
+                MessageBox.Show(co.ToString());
+                MessageBox.Show("Non Connecté");
+            }
         }
     }
 }
