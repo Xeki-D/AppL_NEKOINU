44db0e42a3d26c45bd84b2342e77bb6eaa148a52
EmerickD <74702547+Xeki-D@users.noreply.github.com>
2022-05-06 16:20:14 +0200

    Add files via upload

---
+++ WpfApp_NEKOINU/View/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
+
+namespace WpfApp_NEKOINU.View
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string connectionString = "Server=localhost;Database=nekoinu_bdd;Uid=root;Pwd=";
+        bool connecte = false;
+        MySqlConnection connection;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SignInButton_Click(object sender, RoutedEventArgs e)
+        {
+            connection = new MySqlConnection(connectionString);
+            try
+            {
+
+                if (connection.State == System.Data.ConnectionState.Closed)
+                {
+                    connection.Open();
+                    connecte = true;
+                    string query = "Select Count(1) FROM client WHERE ADR_CLIENT=@adr AND MDP_CLIENT=@mdp";
+                    MySqlCommand cmd = new MySqlCommand(query, connection);
+                    MySqlParameter adr = new MySqlParameter("@adr", txb_login.Text);
+                    MySqlParameter mdp = new MySqlParameter("@mdp", txb_mdp.Password);
+                    int count = Convert.ToInt32(cmd.ExecuteScalar());
+                    if(count == 1)
+                    {
+
+                       
+                        SelectionWindows select = new SelectionWindows();
+                        select.Show();
+                        this.Close();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Identifiant incorrect");
+                    }
+
+
+                }
+            }
+            catch (Exception co)
+            {
+                MessageBox.Show(co.ToString());
+            }
+
+        }
+
+        private void txb_login_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void txb_mdp_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+       
+    }
+}
---
+++ WpfApp_NEKOINU/View/SelectionWindows.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp_NEKOINU.ViewModel;
+using MySql.Data.MySqlClient;
+
+
+namespace WpfApp_NEKOINU.View
+{
+    /// <summary>
+    /// Logique d'interaction pour SelectionWindows.xaml
+    /// </summary>
+    public partial class SelectionWindows : Window
+    {
+        public SelectionWindows()
+        {
+            InitializeComponent();
+        }
+
+        private void bt_ViewP_Click(object sender, RoutedEventArgs e)
+        {
+            SecondWindow s = new SecondWindow();
+            s.Show();
+        }
+
+        private void bt_ViewP_Click_1(object sender, RoutedEventArgs e)
+        {
+            First t = new First();
+            t.Show();
+        }
+
+        private void bt_AddP_Click(object sender, RoutedEventArgs e)
+        {
+            SecondWindow s = new SecondWindow();
+            s.Show();
+        }
+    }
+}
